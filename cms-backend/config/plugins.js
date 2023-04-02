d0043e82aef6f83e3922210cf1bc8156f4eb352c
Gabriel Warkentin <gabrielwarkentin@csu.fullerton.edu>
2023-04-02 16:37:55 -0700

    note to avoid build errors

--- cms-backend/config/plugins.js
+++ cms-backend/config/plugins.js
@@ -1,8 +1,13 @@
 module.exports = {
     // ...
+    /* comment me back in after running container. Attached a shell to the running instance (easy in vs code)
+    // then:
+    // cd /opt/app/src/plugins/arse && npm install && npm run build
+    // then you can docker compose down, recomment this code, and docker compose up
     'arse': {
       enabled: true,
       resolve: './src/plugins/arse'
     },
+    */
     // ...
   }
