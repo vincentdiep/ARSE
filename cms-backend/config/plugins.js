b44f88de5466a38968ddde2017ab8ed63f7edad2
Gabriel Warkentin <gabriel.warkentin@gmail.com>
2023-04-09 23:41:29 -0700

    Reactivizing the drawing squares JS. This was we can integrate with the UI.
    Co-authored-by: Alan Cortez <AlanCortez1337@users.noreply.github.com>

--- cms-backend/config/plugins.js
+++ cms-backend/config/plugins.js
@@ -1,6 +1,6 @@
 module.exports = {
     // ...
-    /* comment me back in after running container. Attached a shell to the running instance (easy in vs code)
+    // comment me back in after running container. Attached a shell to the running instance (easy in vs code)
     // then:
     // cd /opt/app/src/plugins/arse && npm install && npm run build
     // then you can docker compose down, recomment this code, and docker compose up
@@ -8,6 +8,5 @@ module.exports = {
       enabled: true,
       resolve: './src/plugins/arse'
     },
-    */
     // ...
   }
